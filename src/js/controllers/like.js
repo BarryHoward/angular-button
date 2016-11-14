3b4bcf574aeb6171beb4fba795a6b46497b3bf8e
Barry Howard <BarryLeeHoward@gmail.com>
2016-11-14 15:01:59 -0500

    got plurals, toggle, and styling to work

--- src/js/controllers/like.js
+++ src/js/controllers/like.js
@@ -1,8 +1,35 @@
 function LikeController ($scope){
 	$scope.count = 0;
+	$scope.string = "Likes: " + $scope.count;
+	$scope.up = true;
+	$scope.upString = "Up";
 
 	$scope.countUp = function (){
-		$scope.count++;
+
+		if ($scope.up){
+			$scope.count++;
+		} else {
+			$scope.count--;
+		}
+
+		if ($scope.count<0){
+			$scope.count = 0;
+		}
+
+		if ($scope.count === 1){
+			$scope.string = "Like: " + $scope.count;			
+		} else {
+			$scope.string = "Likes: " + $scope.count;
+		}
+	}
+
+	$scope.toggle = function(){
+		$scope.up = !$scope.up;
+		if ($scope.up){
+			$scope.upString = "Up";
+		} else {
+			$scope.upString = "Down";
+		}
 	}
 
 }
